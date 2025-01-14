931d72d193ba2a16cc133cfcd748a12772c0568f
Aart Goossens <aart@goossens.me>
2025-01-14 09:32:32 +0100

    Imports SweatStack library for assessment

--- tests/test_fit.py
+++ tests/test_fit.py
@@ -2,6 +2,9 @@ from endureio.fit import read_fit
 
 
 def test_read_fit_running_file():
-    file_path = "tests/data/running.fit"
+    file_path = "tests/data/short-run.fit"
 
     data = read_fit(file_path)
+
+    assert "power" in data.columns
+    assert len(data) == 1344
